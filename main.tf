fd42fd9b625ab3e55130ee63a385c0dc7f8499d6
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2023-12-29 00:31:02 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 # main.tf
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
+  access_key = "AKIAXKT7HXXUAECI7A6X" 
+  secret_key = "VSCiCbrPPzXnPQ2ZJJACtsyg5UbPRdmT6dEv06ga"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 LTS
+resource "aws_instance" "awsserverinstance" {
+  ami           = "ami-0912f71e06545ad88"  
   instance_type = "t2.micro"
 
   tags = {
-    Name = "example-instance"
+    Name = "awsserverinstance"
   }
 }
