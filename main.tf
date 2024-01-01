3d81b47145188ac927f294585bbe91580029ac4c
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-01 14:41:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@
 
 provider "aws" {
   region = "ap-south-1"
-  //access_key = "AKIAXKT7HXXUGK4SPNFE" 
-  //secret_key = "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"
-}
+  }
 
 resource "aws_instance" "awsserverinstance" {
   ami           = "ami-0912f71e06545ad88"  
