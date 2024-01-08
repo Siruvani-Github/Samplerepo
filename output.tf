f94582a66b0122384338d791ff12c93c0e7b8f6b
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-08 20:36:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output aws_instance_id {
 description = "AWS EC2 instance id"
 value = aws_instance.awsserverinstance.id
 }
-output aws_instance_name {
-description = "AWS EC2 Name"
-value = aws_instance.awsserverinstance.name
+output aws_region.sh {
+description = "AWS EC2 region"
+value = aws_instance.awsserverinstance.region
 }
