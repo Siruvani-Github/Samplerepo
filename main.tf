9156b40789933af76457ab3c0f854273ef419021
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-08 19:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,8 @@ provider "aws" {
 resource "aws_instance" "awsserverinstance" {
   ami           = "ami-0912f71e06545ad88"  
   instance_type = "t2.micro"
-  count = 2
-
-  tags = {
+  
+tags = {
     Name = "awsserverinstance"
   }
 }
