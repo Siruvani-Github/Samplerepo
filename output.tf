7226c3f922f22ba66b5bb780add4d4fd23e920c9
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-08 20:55:59 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output aws_instance_id {
 description = "AWS EC2 instance id"
 value = aws_instance.awsserverinstance.id
 }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
