c35a04438fb9b022a26f130f4ee666e805aac079
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-08 20:23:08 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output aws_instance_id {
 description = "AWS EC2 instance id"
 value = aws_instance.awsserverinstance.id
 }
+output aws_instance_name {
+description = "AWS EC2 Name"
+value = aws_instance.awsserverinstance.name
+}
