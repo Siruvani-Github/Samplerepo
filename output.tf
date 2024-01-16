3d1e45f22b4146658b2e75a135764dfe8d563cf6
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-16 09:19:17 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,12 +1,12 @@
-# output "instance_ip_addresses" {
+ output "instance_ip_addresses" {
   # Result is a map from instance id to private and public IP addresses, such as:  #  {"i-1234" = ["192.168.1.2","54.234.188.251,] "i-5678" = ["192.168.1.5","3.90.189.190",] }
-  # value = {
-   # for instance in aws_instance.awsserverinstance:
-    #  instance.id => instance.private_ip
- # }
-# }
+   value = {
+    for instance in aws_instance.awsserverinstance:
+      instance.id => instance.private_ip
+  }
+ }
 
-output "instance_ip_address_method_2" {
+# output "instance_ip_address_method_2" {
  # type        = string
-  value       = element([for v in values(aws_instance.awsserverinstance) : v.public_ip], 0)
-}
+  # value       = element([for v in values(aws_instance.awsserverinstance) : v.public_ip], 0)
+# }
