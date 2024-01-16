399919eec1059cde93b3b1e83139a54d8048e9b0
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-16 08:48:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,15 @@
-# main.tf
 
 provider "aws" {
   region = "ap-south-1"
   }
 
-resource "aws_instance" "awsserverinstance" {
+resource "aws_instance" "server" {
+  count = var.instances
+
   ami           = "ami-0912f71e06545ad88"  
   instance_type = var.instance_type
   
-tags = {
-    Name = "awsserverinstance"
+  tags = {
+    Name = "Server ${count.index}"
   }
 }
