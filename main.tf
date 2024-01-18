f944dc224cfd66ac533b847edca555aeeeeefc5c
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-18 17:33:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,13 +13,4 @@ resource "aws_instance" "awsserverinstance" {
     Name = "Server ${count.index}"
   }
 }
-resource "aws_instance" "awsserverinstance_1" {
-  count = var.instances
 
-  ami           = "ami-0912f71e06545ad88"  
-  instance_type = var.instance_type
-  
-  tags = {
-    Name = "Server ${count.index}"
-  }
-}
