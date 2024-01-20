1b7a99bf8b7b2e9b8c457a354cdbe3a0ecab17aa
Siruvani-Github <155034571+Siruvani-Github@users.noreply.github.com>
2024-01-20 10:24:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,4 +16,14 @@
   create_before_destroy = true 
   }
   }
-
+ // resource "aws_instance" "awsserverinstance_1" {
+ // ami           = "ami-0912f71e06545ad88"  
+ // instance_type = var.instance_type
+  
+ // tags = {
+ //  Name = "Server 1"
+ // }
+ // lifecycle  { 
+ // create_before_destroy = true 
+ // }
+ // }
